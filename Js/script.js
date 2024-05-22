8739564c4c86cb672dbf0f97f880571127c75e02
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-22 18:46:37 +0200

    set a costant "team" that contains an array of team's members and their infos

---
+++ Js/script.js
@@ -0,0 +1,61 @@
+
+
+const team = [
+    {
+        member: "Wayne Barnett"[
+            {
+                name: "Wayne",
+                lastName: "Barnett",
+                teamRole: "Founder & CEO",
+                image: "style\img\wayne-barnett-founder-ceo.jpg"
+            }
+        ]
+
+    },
+        {member: "Angela Croll"[
+            {
+                name: "Angela",
+                lastName: "Caroll",
+                teamRole: "Chief Editor",
+                image: "style\img\angela-lopez-social-media-manager.jpg"
+            }
+        ]
+    },
+        {member: "Walter Gordon"[
+            {
+                name: "Walter",
+                lastName: "Gordon",
+                teamRole: "Office Manager",
+                image: "style\img\walter-gordon-office-manager.jpg"
+            }
+        ]
+    },
+        {member: "Angela Lopez"[
+            {
+                name: "Angela",
+                lastName: "Lopez",
+                teamRole: "Social Media Manager",
+                image: "style\img\angela-lopez-social-media-manager.jpg"
+            }
+        ]
+    },
+
+        {member: "Scott Estrada"[
+            {
+                name: "Scott",
+                lastName: "Estrada",
+                teamRole: "Developer",
+                image: "style\img\scott-estrada-developer.jpg"
+            }
+        ]
+    },
+    {member: "Barbara Ramos"[
+            {
+                name: "Barbara",
+                lastName: "Ramos",
+                teamRole: "Graphic Designer",
+                image: "style\img\barbara-ramos-graphic-designer.jpg"
+            }
+        ]
+    }
+]
